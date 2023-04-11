4fbf262e663867870205bfba909eb4f6a3f61adf
egor-davydov <a.egordavidov@gmail.com>
2023-04-11 06:02:15 +0300

    feat: add Outfit and Ammo StaticData

---
+++ src/SurvivalGameTestTask/Assets/Code/StaticData/BulletStaticData.cs
@@ -0,0 +1,11 @@
+﻿using Code.Services.StaticData;
+using UnityEngine;
+
+namespace Code.StaticData
+{
+  [CreateAssetMenu(menuName = "StaticData/Item/Bullet", fileName = "BulletStaticData", order = 0)]
+  public class BulletStaticData : ItemStaticData
+  {
+    public BulletType BulletType;
+  }
+}
